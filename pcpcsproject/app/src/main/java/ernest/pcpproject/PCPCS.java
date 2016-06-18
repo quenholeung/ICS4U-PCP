4e0533c491c578c76ad5deaf75de58e2178cedf8
Ernest Wang <dragongnexus@gmail.com>
2016-06-18 19:14:08 -0400

    start of app

---
+++ pcpcsproject/app/src/main/java/ernest/pcpproject/PCPCS.java
@@ -0,0 +1,13 @@
+package ernest.pcpproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PCPCS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pcpcs);
+    }
+}
